7c4148ff51938206f7420f4ecbe47a9ee5158ae4
housseine <housseine2014tassa@gmail.com>
2018-09-06 15:31:55 +0100

    [refactor] - modification du fichier build.sh

--- src/main/java/ma/craft/craft/trackntrace/controller/Test.java
+++ src/main/java/ma/craft/craft/trackntrace/controller/Test.java
@@ -1,5 +1,14 @@
 package ma.craft.craft.trackntrace.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class Test {
+	 @RequestMapping("/")
+	   public @ResponseBody String greeting() {
+	        return "Ops is On dev tools";
+	    }
 
 }
