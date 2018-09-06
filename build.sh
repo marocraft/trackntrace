e49ef044dc0971175323562b1cebad158ae64842
housseine <housseine2014tassa@gmail.com>
2018-09-06 12:19:36 +0100

    [refactor] - configuration fichier build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
       echo 'Checking Quality Gates'
-      mvn -B clean verify sonar:sonar 
-      -Dsonar.host.url=${SONAR_URL} 
-      -Dsonar.login=49265080d82862cd834c496f52bba352d84db019
-      -Dsonar.projectKey=${SONAR_PROJECT} 
-      -Dsonar.organization=${SONAR_ORGANIZATION}
+      mvn -B clean verify sonar:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.projectKey=${SONAR_PROJECT} -Dsonar.organization=${SONAR_ORGANIZATION}
 fi
