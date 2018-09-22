10550c408c47f7139d6b337b8bda5cc3c47cb997
Sallah Kokaina <sallah.kokaina@gmail.com>
2018-09-22 21:41:20 +0100

    [refactor] - prune and remove unused files

--- src/main/java/ma/craft/trackntrace/domain/LogTrace.java
+++ src/main/java/ma/craft/trackntrace/domain/LogTrace.java
@@ -1,31 +1,15 @@
 package ma.craft.trackntrace.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+        import lombok.*;
 
-@Getter @Setter
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LogTrace {
-	public long executionTime;
-	public String methodName;
-	public String className;
-	public String logLevel;
-
-	public LogTrace() {
-		super();
-
-	}
-
-	public LogTrace(long executionTime, String methodName, String className, String logLevel) {
-		super();
-		this.executionTime = executionTime;
-		this.methodName = methodName;
-		this.className = className;
-		this.logLevel = logLevel;
-	}
-	public int getColumnCount() {
-	    return getClass().getDeclaredFields().length;
-	}
-
-
-
+    private long executionTime;
+    private String methodName;
+    private String className;
+    private String logLevel;
 }
