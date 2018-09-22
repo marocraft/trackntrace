8eb860be35323fb964a5212f071c2d0fdf117442
Sallah Kokaina <sallah.kokaina@gmail.com>
2018-09-22 21:45:31 +0100

    [build] - unused setters

--- src/main/java/ma/craft/trackntrace/generate/LogBuilder.java
+++ src/main/java/ma/craft/trackntrace/generate/LogBuilder.java
@@ -7,18 +7,18 @@ public class LogBuilder {
 	public static String build(LogTrace logTrace) {
 		StringBuffer logMessage = new StringBuffer();
 		logMessage.append("classe : ");
-		logMessage.append(logTrace.className);
+		logMessage.append(logTrace.getClassName());
 		logMessage.append(",");
 		logMessage.append("name : ");
-		logMessage.append(logTrace.methodName);
+		logMessage.append(logTrace.getMethodName());
 		logMessage.append(",");
 		logMessage.append("level :  ");
 		logMessage.append("(");
-		logMessage.append(logTrace.logLevel);
+		logMessage.append(logTrace.getLogLevel());
 		logMessage.append(")");
 		logMessage.append(",");
 		logMessage.append(" execution time: ");
-		logMessage.append(logTrace.executionTime);
+		logMessage.append(logTrace.getExecutionTime());
 		logMessage.append(" ms");
 		return logMessage.toString();
 	}
