d5d86a3c577bd36b2f111f43aa13defd00cfb3d3
housseine <housseine2014tassa@gmail.com>
2019-03-22 16:04:33 +0100

    [snapshot] - private and public key encrypted and added to deploy repo

---
+++ deploy/publish.sh
@@ -0,0 +1,7 @@
+echo "Deploy for tag: $TRAVIS_TAG - branch: $TRAVIS_BRANCH"
+
+if [ $TRAVIS_PULL_REQUEST == "false" ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
+    echo 'Deploying snapshot to OSS'
+    mvn clean deploy --settings ./maven.xml -DskipTests=true -Pdeploy;
+    exit $?;
+fi
