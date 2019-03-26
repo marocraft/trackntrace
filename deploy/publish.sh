ff0953c1c4d600e91cc40070246f41e4115aa6eb
housseine <housseine2014tassa@gmail.com>
2019-03-26 16:24:11 +0100

    [release] - echo the travis branch

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,18 +1,7 @@
 echo "Deploy for tag: $TRAVIS_TAG - branch: $TRAVIS_BRANCH pr: $TRAVIS_PULL_REQUEST_BRANCH"
 echo "Deploy for tag: $TRAVIS_TAG - branch: ${TRAVIS_BRANCH}"
-
-if [ ${TRAVIS_PULL_REQUEST} = 'true' ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
-    echo 'Deploying snapshot to OSS 1'
-fi
-if [ ${TRAVIS_PULL_REQUEST} = 'true' ] && [ ${TRAVIS_BRANCH} != 'snapshot' ]; then
-    echo 'Deploying snapshot to OSS 2'
-fi
-if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
-    echo 'Deploying snapshot to OSS 3'
-fi
-if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_BRANCH} != 'snapshot' ]; then
-    echo "Deploying snapshot to OSS 4  pr: $TRAVIS_PULL_REQUEST_BRANCH"
-fi
+export BRANCH=$TRAVIS_BRANCH
+echo "the travis branch is: $BRANCH"
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
     echo 'Deploying snapshot to OSS'
     mvn clean deploy --settings ./maven.xml -DskipTests=true -Pdeploy;
