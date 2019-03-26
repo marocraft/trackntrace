53c9a28e81b776528ebb021dbdaa583343816010
housseine <housseine2014tassa@gmail.com>
2019-03-26 10:46:42 +0100

    [release] - fix build

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,7 +1,6 @@
 echo "Deploy for tag: $TRAVIS_TAG - branch: $TRAVIS_BRANCH"
-echo ${TRAVIS_BRANCH}
 
-if [ $TRAVIS_PULL_REQUEST == "false" ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
+if [ ${TRAVIS_PULL_REQUEST} == 'false' ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
     echo 'Deploying snapshot to OSS'
     mvn clean deploy --settings ./maven.xml -DskipTests=true -Pdeploy;
     exit $?;
