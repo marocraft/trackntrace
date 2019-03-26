ea2a86d38eabb5aedcdccb11546d2492cef43364
housseine <housseine2014tassa@gmail.com>
2019-03-26 23:34:36 +0100

    [release] - speifeid path to maven.xml

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -2,6 +2,6 @@ echo "Deploy for tag: $TRAVIS_TAG - branch: $TRAVIS_BRANCH"
 
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
     echo 'Deploying snapshot to OSS'
-    mvn clean deploy --settings maven.xml -DskipTests=true -Pdeploy;
+    mvn clean deploy --settings ../.maven.xml -DskipTests=true -Pdeploy;
     exit $?;
 fi
