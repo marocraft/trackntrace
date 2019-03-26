15b11af421decc2008b795ac84ee270d1743d20f
housseine <housseine2014tassa@gmail.com>
2019-03-26 23:17:26 +0100

    [release] - echo the travis branch

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,9 +1,7 @@
-echo "Deploy for tag: $TRAVIS_TAG - branch: $TRAVIS_BRANCH pr: $TRAVIS_PULL_REQUEST_BRANCH"
-echo "Deploy for tag: $TRAVIS_TAG - branch: ${TRAVIS_BRANCH}"
-export BRANCH=$TRAVIS_BRANCH
-echo "the travis branch is: $BRANCH"
+echo "Deploy for tag: $TRAVIS_TAG - branch: $TRAVIS_BRANCH"
+
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
     echo 'Deploying snapshot to OSS'
-    mvn clean deploy --settings ./maven.xml -DskipTests=true -Pdeploy;
+    mvn clean deploy --settings ../maven.xml -DskipTests=true -Pdeploy;
     exit $?;
 fi
