ac44567bb84009248cf997ea68b683ac67d7887e
housseine <housseine2014tassa@gmail.com>
2019-03-27 17:04:20 +0100

    [release] - try to sign artifact

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,7 +1,7 @@
 echo "Deploy for tag: $TRAVIS_TAG - branch: $TRAVIS_BRANCH"
 
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
-    echo 'Deploying snapshot to OSS'
+    echo 'Deploying snapshot to OSS in branch ${TRAVIS_BRANCH}'
     mvn clean deploy --settings ./.maven.xml -DskipTests=true -Possrh;
     exit $?;
 fi
