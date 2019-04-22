d69107976a17fdb40bcbe2694342c72572d08050
housseine <housseine2014tassa@gmail.com>
2019-04-22 10:34:19 +0100

    Revert "[snapshot] - deploy snapshot"
    This reverts commit d904d5c7dcba2a14bfd5857cae4876166242b23b.

--- src/test/java/ma/craft/trackntrace/AspectAnnotationITest.java
+++ src/test/java/ma/craft/trackntrace/AspectAnnotationITest.java
@@ -38,8 +38,8 @@ public class AspectAnnotationITest {
 	public void shoulTakeMessage() throws IOException, InterruptedException, FileNotFoundException {
 		loggerThread= new LoggerThread();
 		logPublisher.publish("my log");
-		
+		Assert.assertEquals(1, logPublisher.size());
 		logPublisher.clear();
-		
+		Assert.assertEquals(0, logPublisher.size());
 	}
 }
