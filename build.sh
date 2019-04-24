5151d75bc4853f2a123ec1d057c0e1ccd78f5d71
Housseine Tassa <housseine.tassa@bnpparibas.com>
2019-04-24 16:42:22 +0100

    [build] - build fixed

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ ${TRAVIS_BRANCH} = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
       echo 'Checking Quality Gates'
-      mvn -B clean verify sonar:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.projectKey=${SONAR_PROJECT} -Dsonar.organization=${SONAR_ORGANIZATION}
+      mvn -B clean verify sonar:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.projectKey=${SONAR_PROJECT} -Dsonar.organization=${SONAR_ORGANIZATION} -X
 	  
 		
 elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then 
