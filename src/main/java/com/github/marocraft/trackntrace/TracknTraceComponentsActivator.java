7f142d74541cb34705f03ce1e957203de56113d4
b31402 <khalid.elabbadi@externe.bnpparibas.com>
2019-04-24 11:29:44 +0100

    [refactoring] solid c1

---
+++ src/main/java/com/github/marocraft/trackntrace/TracknTraceComponentsActivator.java
@@ -0,0 +1,11 @@
+package com.github.marocraft.trackntrace;
+
+/**
+ * Locator interface for Spring context component scanning.
+ *
+ * @author Khalid ELABBADI
+ * @since 0.0.3
+ */
+public interface TracknTraceComponentsActivator {
+
+}
--- src/main/java/com/github/marocraft/trackntrace/annotation/EnableTracknTrace.java
+++ src/main/java/com/github/marocraft/trackntrace/annotation/EnableTracknTrace.java
@@ -1,24 +1,28 @@
 package com.github.marocraft.trackntrace.annotation;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.marocraft.trackntrace.TracknTraceComponentsActivator;
 
 /**
  * Enables TacknTrace on the target project. This annotation must be used on the
  * project main class
  * 
  * @author Khalid ELABBADI
- * @since 0.0.4
+ * @since 0.0.3
  */
-@Documented
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@ComponentScan("com.github.marocraft.trackntrace.*")
+@Retention(value = RUNTIME)
+@Target(value = { TYPE })
+@Configuration
+@ComponentScan(basePackageClasses = TracknTraceComponentsActivator.class)
+@ComponentScan(basePackages = "com.github.marocraft.trackntrace")
 public @interface EnableTracknTrace {
 
 }
