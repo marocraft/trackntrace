9753657152985e3ce54ced9d2443d82126566ed3
b31402 <khalid.elabbadi@externe.bnpparibas.com>
2019-04-26 11:31:45 +0100

    [refact] refact and add correlation id tracking mechanism

---
+++ src/main/java/com/github/marocraft/trackntrace/PackageName.java
@@ -0,0 +1,17 @@
+package com.github.marocraft.trackntrace;
+
+/**
+ * Constants for used packages
+ * 
+ * @author Khalid ELABBADI
+ *
+ */
+public class PackageName {
+
+	private PackageName() {
+
+	}
+
+	public static final String ROOT = "com.github.marocraft.trackntrace";
+	public static final String HTTP = "com.github.marocraft.trackntrace.http";
+}
