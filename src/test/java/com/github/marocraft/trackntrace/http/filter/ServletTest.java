c9baaf68f057ef8afa7a8dc17ca16a34ba672c06
housseine <housseine2014tassa@gmail.com>
2019-08-08 16:50:01 +0100

    [UT] - added ut for HttpLogsFilter

--- src/test/java/com/github/marocraft/trackntrace/http/filter/ServletTest.java
+++ src/test/java/com/github/marocraft/trackntrace/http/filter/ServletTest.java
@@ -18,8 +18,13 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet()
 public class ServletTest extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		doGet(request, response);
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		//doGet(request, response);
 	}
 }
