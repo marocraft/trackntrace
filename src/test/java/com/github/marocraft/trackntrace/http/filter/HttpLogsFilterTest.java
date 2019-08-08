83abe325139b88555dc18726c4d6fa3d7c08ec6e
housseine <housseine2014tassa@gmail.com>
2019-08-08 16:50:01 +0100

    [UT] - added ut for HttpLogsFilter

--- src/test/java/com/github/marocraft/trackntrace/http/filter/HttpLogsFilterTest.java
+++ src/test/java/com/github/marocraft/trackntrace/http/filter/HttpLogsFilterTest.java
@@ -9,11 +9,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
-
-import junit.framework.Assert;
 
 /**
  * 
@@ -40,20 +39,17 @@ public class HttpLogsFilterTest {
 	 */
 	@Test
 	public void shouldGetPublicIpAdress() {
-//		MutableHttpServletRequest mutableHttpServletRequest=new MutableHttpServletRequest(httpServletRequest);
-//		mutableHttpServletRequest.putHeader("x-forwarded-for", "10.78.243.194");
-//		try {
-//			servlet.doPost(httpServletRequest, httpServletResponse);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (ServletException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		System.out.println(httpServletRequest.getHeader("x-forwarded-for"));
-//		
-		//Assert.assertEquals("10.78.243.194", httpLogsFilter.getPublicIpAdress(httpServletRequest));
+		MutableHttpServletRequest mutableHttpServletRequest=new MutableHttpServletRequest(httpServletRequest);
+		mutableHttpServletRequest.putHeader("x-forwarded-for", "10.78.243.194");
+		try {
+			servlet.doGet(mutableHttpServletRequest, httpServletResponse);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
+		System.out.println(mutableHttpServletRequest.getHeader("x-forwarded-for"));
+		Assert.assertEquals("10.78.243.194", httpLogsFilter.getPublicIpAdress(mutableHttpServletRequest));
 		
 	}
 
