3f206213ab923a5182deae30b0ad51d65174f358
Housseine Tassa <housseine.tassa@bnpparibas.com>
2019-08-09 11:16:17 +0100

    [UT] - added unit tests for class Log

--- src/test/java/com/github/marocraft/trackntrace/http/filter/MutableHttpServletRequest.java
+++ src/test/java/com/github/marocraft/trackntrace/http/filter/MutableHttpServletRequest.java
@@ -18,44 +18,36 @@ import javax.servlet.http.HttpServletRequestWrapper;
  *
  */
 final class MutableHttpServletRequest extends HttpServletRequestWrapper {
-    // holds custom header and value mapping
-    private final Map<String, String> customHeaders;
- 
-    public MutableHttpServletRequest(HttpServletRequest request){
-        super(request);
-        this.customHeaders = new HashMap<String, String>();
-    }
-    
-    public void putHeader(String name, String value){
-        this.customHeaders.put(name, value);
-    }
- 
-    public String getHeader(String name) {
-        // check the custom headers first
-        String headerValue = customHeaders.get(name);
-        
-        if (headerValue != null){
-            return headerValue;
-        }
-        // else return from into the original wrapped object
-        return ((HttpServletRequest) getRequest()).getHeader(name);
-    }
- 
-    public Enumeration<String> getHeaderNames() {
-        // create a set of the custom header names
-        Set<String> set = new HashSet<String>(customHeaders.keySet());
-        
-        // now add the headers from the wrapped request object
-        @SuppressWarnings("unchecked")
-        Enumeration<String> e = ((HttpServletRequest) getRequest()).getHeaderNames();
-        while (e.hasMoreElements()) {
-            // add the names of the request headers into the list
-            String n = e.nextElement();
-            set.add(n);
-        }
- 
-        // create an enumeration from the set and return
-        return Collections.enumeration(set);
-    }
-}
+	private final Map<String, String> customHeaders;
 
+	public MutableHttpServletRequest(HttpServletRequest request) {
+		super(request);
+		this.customHeaders = new HashMap<String, String>();
+	}
+
+	public void putHeader(String name, String value) {
+		this.customHeaders.put(name, value);
+	}
+
+	public String getHeader(String name) {
+		String headerValue = customHeaders.get(name);
+
+		if (headerValue != null) {
+			return headerValue;
+		}
+		return ((HttpServletRequest) getRequest()).getHeader(name);
+	}
+
+	public Enumeration<String> getHeaderNames() {
+		Set<String> set = new HashSet<String>(customHeaders.keySet());
+
+		@SuppressWarnings("unchecked")
+		Enumeration<String> e = ((HttpServletRequest) getRequest()).getHeaderNames();
+		while (e.hasMoreElements()) {
+			String n = e.nextElement();
+			set.add(n);
+		}
+
+		return Collections.enumeration(set);
+	}
+}
