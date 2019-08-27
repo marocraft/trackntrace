5913b8ef5f1c121199ef9a30d474f3ea1561ec39
Housseine Tassa <housseine.tassa@bnpparibas.com>
2019-08-27 09:08:49 +0100

    [UT] - added unit tests

--- src/test/java/com/github/marocraft/trackntrace/http/filter/HttpLogsFilterTest.java
+++ src/test/java/com/github/marocraft/trackntrace/http/filter/HttpLogsFilterTest.java
@@ -23,23 +23,24 @@ public class HttpLogsFilterTest {
 	HttpServletResponse httpServletResponse;
 	HttpLogsFilter httpLogsFilter;
 	TestServlet servlet;
-	
+	MutableHttpServletRequest mutableHttpServletRequest;
+
 	@Before
 	public void before() {
-	httpServletRequest=Mockito.mock(HttpServletRequest.class);
-	httpServletResponse=Mockito.mock(HttpServletResponse.class);
-	httpLogsFilter= new HttpLogsFilter();
-	servlet= new TestServlet();
-	
+		httpServletRequest = Mockito.mock(HttpServletRequest.class);
+		httpServletResponse = Mockito.mock(HttpServletResponse.class);
+		httpLogsFilter = new HttpLogsFilter();
+		servlet = new TestServlet();
+
 	}
-	
 
 	/**
-	 * Test method for {@link com.github.marocraft.trackntrace.http.filter.HttpLogsFilter#getPublicIpAdress(javax.servlet.http.HttpServletRequest)}.
+	 * Test method for
+	 * {@link com.github.marocraft.trackntrace.http.filter.HttpLogsFilter#getPublicIpAdress(javax.servlet.http.HttpServletRequest)}.
 	 */
 	@Test
-	public void shouldGetPublicIpAdress() {
-		MutableHttpServletRequest mutableHttpServletRequest=new MutableHttpServletRequest(httpServletRequest);
+	public void shouldGetPublicIpAdressFromXForwardedFor() {
+		mutableHttpServletRequest = new MutableHttpServletRequest(httpServletRequest);
 		mutableHttpServletRequest.putHeader("x-forwarded-for", "10.78.243.194");
 		try {
 			servlet.doGet(mutableHttpServletRequest, httpServletResponse);
@@ -48,9 +49,72 @@ public class HttpLogsFilterTest {
 		} catch (ServletException e) {
 			e.printStackTrace();
 		}
-		System.out.println(mutableHttpServletRequest.getHeader("x-forwarded-for"));
 		Assert.assertEquals("10.78.243.194", httpLogsFilter.getPublicIpAdress(mutableHttpServletRequest));
-		
+
 	}
+
+	/**
+	 * Test method for
+	 * {@link com.github.marocraft.trackntrace.http.filter.HttpLogsFilter#getPublicIpAdress(javax.servlet.http.HttpServletRequest)}.
+	 */
+	@Test
+	public void shouldGetPublicIpAdressFromProxyClientIP() {
+		mutableHttpServletRequest = new MutableHttpServletRequest(httpServletRequest);
+		mutableHttpServletRequest.putHeader("x-forwarded-for", null);
+		mutableHttpServletRequest.putHeader("Proxy-Client-IP", "10.78.243.193");
+		try {
+			servlet.doGet(mutableHttpServletRequest, httpServletResponse);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
+
+		
+		Assert.assertEquals("10.78.243.193", httpLogsFilter.getPublicIpAdress(mutableHttpServletRequest));
+	}
+	
+	/**
+	 * Test method for
+	 * {@link com.github.marocraft.trackntrace.http.filter.HttpLogsFilter#getPublicIpAdress(javax.servlet.http.HttpServletRequest)}.
+	 */
+	@Test
+	public void shouldGetPublicIpAdressFromWLProxyClientIP() {
+		mutableHttpServletRequest = new MutableHttpServletRequest(httpServletRequest);
+		mutableHttpServletRequest.putHeader("x-forwarded-for", null);
+		mutableHttpServletRequest.putHeader("Proxy-Client-IP", null);
+		mutableHttpServletRequest.putHeader("WL-Proxy-Client-IP", "10.78.243.193");
+		try {
+			servlet.doGet(mutableHttpServletRequest, httpServletResponse);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals("10.78.243.193", httpLogsFilter.getPublicIpAdress(mutableHttpServletRequest));
+	}	
+	
+	
+	/**
+	 * Test method for
+	 * {@link com.github.marocraft.trackntrace.http.filter.HttpLogsFilter#getPublicIpAdress(javax.servlet.http.HttpServletRequest)}.
+	 */
+	@Test
+	public void shouldGetPublicIpAdressFromRemote() {
+		mutableHttpServletRequest = new MutableHttpServletRequest(httpServletRequest);
+		mutableHttpServletRequest.putHeader("x-forwarded-for", null);
+		mutableHttpServletRequest.putHeader("Proxy-Client-IP", null);
+		mutableHttpServletRequest.putHeader("WL-Proxy-Client-IP", null);
+		try {
+			servlet.doGet(mutableHttpServletRequest, httpServletResponse);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(mutableHttpServletRequest.getRemoteAddr(), httpLogsFilter.getPublicIpAdress(mutableHttpServletRequest));
+	}	
 
 }
